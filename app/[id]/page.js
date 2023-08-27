c5f9c41d86be0f68e5c457c2d5053817cc7c8841
Akash Bag <110753356+akash0708@users.noreply.github.com>
2023-08-27 18:15:09 +0530

    responsiveness added

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { AddToCalendarButton } from "add-to-calendar-button-react";
+import Navbar from "@components/Navbar";
+import Footer from "@components/Footer";
 
 async function getData() {
   const res = await fetch("https://gdscdev.vercel.app/api");
@@ -24,55 +26,54 @@ const page = async ({ params }) => {
 
   return (
     <>
-      <div className="relative bg-blue-900 w-full h-96 mx-auto text-center">
-        <div className="container w-2/3 h-fit relative mx-auto top-36 rounded-3xl overflow-hidden bg-red-500">
-          <div className="image_box relative w-full h-fit mx-auto">
+      <Navbar />
+      <div className="w-full h-fit">
+        <div className="relative bg-blue-900 w-full h-96 mx-auto text-center"></div>
+        <div className="container sm:w-2/3 w-4/5 h-fit relative mx-auto pb-6 sm:-top-48 -top-60 rounded-3xl overflow-hidden bg-white border-2">
+          <div className="image_box relative w-full h-64 sm:h-fit mx-auto">
             <img
               src={cardData.banner_image}
               className="relative w-full h-full"
               alt=""
             />
           </div>
-          <div className="details realtive w-full h-fit text-left px-12">
-            <p className="relative text-5xl font-Poppins font-extrabold py-12">
+          <div className="details realtive w-full h-fit sm:text-left text-center sm:px-12 px-8">
+            <p className="relative sm:text-5xl text-4xl font-Poppins font-extrabold py-12">
               {cardData.title}
             </p>
-            <p className="relative text-4xl font-Poppins mb-6 font-semibold">
+            <p className="relative sm:text-4xl text-3xl text-[#61677A] font-Poppins mb-6 font-semibold">
               Description
             </p>
-            <p className="relative pb-12 font-Poppins">
+            <p className="relative pb-12 font-Poppins text-[#6B728E]">
               {cardData.description}
             </p>
-            <p className="relative text-4xl font-Poppins mb-6 font-semibold">
+            <p className="relative sm:text-4xl text-3xl text-[#61677A] font-Poppins mb-6 font-semibold">
               Organiser
             </p>
             <div>
-              <p className="relative pb-12 font-Poppins">
+              <p className="relative pb-12 font-Poppins text-[#6B728E]">
                 {cardData.organiser_name}
               </p>
             </div>
-            <p className="relative text-4xl font-Poppins mb-6 font-semibold">
+            <p className="relative sm:text-4xl text-3xl text-[#61677A] font-Poppins mb-6 font-semibold">
               Date and Time
             </p>
-            <p className="relative pb-12 font-Poppins">
+            <p className="relative pb-12 font-Poppins text-[#6B728E]">
               {eventDate.toLocaleString()}
             </p>
-            <div className="relative bg-yellow-400">
-              hello
-              <AddToCalendarButton
-                name="Title"
-                options={["Apple", "Google"]}
-                location="World Wide Web"
-                startDate="2023-08-30"
-                endDate="2023-08-30"
-                startTime="10:15"
-                endTime="23:30"
-                timeZone="America/Los_Angeles"
-              ></AddToCalendarButton>
-            </div>
+            <p className="relative sm:text-4xl text-3xl text-[#61677A] font-Poppins mb-6 font-semibold">
+              Venue
+            </p>
+            <p className="relative mb-2 font-Poppins text-[#6B728E]">
+              At {cardData.venue_name}
+            </p>
+            <p className="relative mb-2 font-Poppins text-[#6B728E]">
+              {cardData.venue_city}, {cardData.venue_country}
+            </p>
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Navbar from "@components/Navbar";
+import Footer from "@components/Footer";
 
 async function getData() {
   const res = await fetch("https://gdscdev.vercel.app/api");
@@ -21,38 +23,46 @@ export default async function Page() {
   // console.log(card);
 
   return (
-    <main className="felx flex-col items-center justify-center text-center">
-      {card.map((current) => {
-        return (
-          <div className="relative text-center my-12 mx-auto w-96 h-[30rem] bg-[#F6F1E9] rounded-2xl overflow-hidden">
-            <div className="absolute flex justify-center items-center top-56 left-36 w-24 h-24 bg-white z-10 rounded-full">
-              <img
-                src={current.organiser_icon}
-                className="relative w-11/12 h-11/12"
-                alt=""
-              />
-            </div>
-            <div className="relative w-full h-3/5">
-              <img
-                src={current.banner_image}
-                className="relative w-full h-full"
-                alt="banner_image"
-              />
-            </div>
-            <div className="relative flex flex-col justify-center items-center my-6">
-              <Link href={`/${current.id}`}>
-                <h1 className="relative font-extrabold text-xl my-4 font-Poppins">
+    <>
+      <Navbar />
+      <main className="grid grid-cols-1 sm:grid-cols-3 gap-4 w-4/5 mx-auto">
+        {card.map((current) => {
+          return (
+            <div className="relative text-center my-12 mx-auto w-72 sm:w-96 h-[34rem] pb-4 bg-[#F6F1E9] rounded-2xl overflow-hidden">
+              <div className="absolute flex justify-center items-center top-1/2 sm:left-[40%] left-1/3 w-24 h-24 bg-white z-10 rounded-full">
+                <img
+                  src={current.organiser_icon}
+                  className="relative w-11/12 h-11/12"
+                  alt=""
+                />
+              </div>
+              <div className="relative w-full h-3/5">
+                <img
+                  src={current.banner_image}
+                  className="relative w-full h-full"
+                  alt="banner_image"
+                />
+              </div>
+              <div className="relative flex flex-col justify-center items-center my-6">
+                <h1 className="relative font-extrabold text-xl mt-7 mb-2 font-Poppins">
                   {current.title}
                 </h1>
-              </Link>
-              <p className="font-Roboto">{`${new Date(
-                current.date_time
-              ).toDateString()}`}</p>
-              <p className="relative font-bold font-Poppins my-3">{`${current.venue_city}, ${current.venue_country}`}</p>
+                <p className="font-Roboto">{`${new Date(
+                  current.date_time
+                ).toDateString()}`}</p>
+                <p className="relative font-bold font-Poppins my-3">{`${current.venue_city}, ${current.venue_country}`}</p>
+                <Link
+                  href={`/${current.id}`}
+                  className="realtive rounded-xl bg-[#0B666A] hover:bg-[#35A29F] transition duration-300 text-white font-Poppins w-3/5 py-2 my-2"
+                >
+                  Learn More
+                </Link>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </main>
+          );
+        })}
+      </main>
+      <Footer />
+    </>
   );
 }
